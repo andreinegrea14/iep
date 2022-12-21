9e72c03ab892010812a20982eed78a3569c4702c
andreinegrea14 <andrei.negrea14@yahoo.com>
2022-12-21 23:49:14 +0200

    adaugare cod lucrarea 6

--- plane.cpp
+++ plane.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 using namespace std;
+
+#define Lock_Code "";
+
 // bbase
 class Uncopyable
 {
@@ -16,6 +19,8 @@ private:
 //: private Uncopyable
 class Plane
 {
+protected:
+    bool isLocked = false;
 
 public:
     Plane() : fuelLevel(0), distance(0), planeName("not_plane") {}
@@ -44,9 +49,16 @@ public:
         return *this;
     }
 
-    void planeInfo()
+    string planeInfo()
     {
-        cout << "The plane " << planeName << " traveled " << distance << " km and has " << *fuelLevel << " litres of kerosen left.\n";
+        if (isLocked)
+        {
+            return "Lock_Code";
+        }
+        else
+        {
+            cout << "The plane " << planeName << " traveled " << distance << " km and has " << *fuelLevel << " litres of kerosen left.\n";
+        }
     }
 
     ~Plane()
@@ -54,6 +66,15 @@ public:
         cout << "The Plane does not exist in the memory.\n";
     }
 
+    void lock()
+    {
+        this->isLocked = true;
+    }
+
+    void unLock()
+    {
+        this->isLocked = false;
+    }
     // pt custom copy asignment
     //  Plane& operator=(const Plane& Plane){
     //    this->fuelLevel=Plane.fuelLevel;
@@ -112,6 +133,48 @@ private:
     int seats;
 };
 
+class LockPlane
+{
+private:
+    Plane &lockedPlane;
+
+public:
+    explicit LockPlane(Plane &value) : lockedPlane(value)
+    {
+        lock();
+    }
+
+    ~LockPlane()
+    {
+        unlock();
+    }
+};
+
+Plane *createPlaneInstance(int fuelLevel, int distance, string planeName)
+{
+    return (new Plane(fuelLevel, distance, planeName));
+}
+
+void doSomething()
+{
+    Plane *p = createPlaneInstance(123, 451, "Boeing X");
+    return;
+    delete p;
+    unique_ptr<Plane> newPlane(createPlaneInstance(123, 455, "Boeing Y"));
+    newPlane->planeInfo();
+
+    unique_ptr<Plane> newPlane2 = move(newPlane);
+
+    shared_ptr<Plane> p2(createPlaneInstance(1255, 4212, "Boeing Z"));
+    shared_ptr<Plane> p3(p2);
+    p2->planeInfo();
+    p3->planeInfo();
+    p2.use_count();
+
+    shared_ptr<Plane> p4 = move(p2);
+    p4->planeInfo();
+}
+
 int main()
 {
     Plane Plane1(99, 1001, "Boeing 747");
@@ -124,5 +187,16 @@ int main()
     Boeing boeing(55, 129, "Boeing 737", 180);
     boeing.planeInfo();
     cout << "Latest boeing has " << boeing.getNumberOfSeats() << " number of seats.\n";
+
+    doSomething();
+
+    Plane Plane5(555, 421, "Boeing A");
+    LockPlane *lockedPlane = new LockPlane(Plane5);
+
+    Plane5.planeInfo();
+
+    delete lockedPlane;
+    Plane5.planeInfo();
+
     return 0;
 }
